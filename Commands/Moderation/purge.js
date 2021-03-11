d4bdc6724da4141819db8e65152dc3a26f8245db
Habelito <79597197+Habelito@users.noreply.github.com>
2021-03-11 09:16:39 -0600

    Create purge.js

---
+++ Commands/Moderation/purge.js
@@ -0,0 +1,19 @@
+if(message.content.startsWith("purge")){
+let arg = message.content.split(" ")
+if(message.member.hasPermission("MANAGE_MESSAGES")) {
+let clear = arg[1];
+if(!clear) return message.channel.send(`:x: | \`Incorrect usage of command you need to provide an amount of messages to Clear.\` 
+**Example:** \`_purge 50\` `)
+if(isNaN(clear)) return message.channel.send(":x: | ``Please Put a Valid Number to Clear messages.``")
+if(clear > 100) return message.channel.send(":x: | ``I can't Clear more than 100 messages.``")
+if(clear < 1) return message.channel.send(":x: | ``You cannot Clear less than 1 message.``")
+
+message.channel.bulkDelete(clear)
+message.channel.send(`:white_check_mark: | \`Succesfully cleared ${clear} messages! | If purge fails please make sure I have MANAGE_MESSAGES to make the purge seccessful.\` `)
+.then(message => 
+ message.delete({timeout: 10000})
+ )
+}else{
+message.reply("You dont have perms!")
+} 
+}
